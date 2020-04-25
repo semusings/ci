dae1e186eb1a1a1f82cacd179e1a798859d583b8
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-04-25 17:29:01 +0545

    fix: test semantic release

--- import-gpg.sh
+++ import-gpg.sh
@@ -4,3 +4,9 @@ gpg --import mypubkeys.asc
 gpg -K
 gpg -k
 gpg --import-ownertrust otrust.txt
+
+KEY_ID=$(gpg --list-secret-keys | head -4 | tail -1 | sed "s/ //g")
+
+echo "Key Id: $KEY_ID"
+
+gpg --keyserver hkp://pool.sks-keyservers.net --send-keys "$KEY_ID"
